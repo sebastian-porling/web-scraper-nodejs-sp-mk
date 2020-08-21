62de96ccc5b85c448a3f0bd11e2ce22d6c2b0479
Sebastian Porling <sebastian.porling@gmail.com>
2020-08-21 23:52:28 +0200

    feat: added formating for writing json

--- modules/fileWriter.js
+++ modules/fileWriter.js
@@ -1,9 +1,10 @@
 const jsonfile = require("jsonfile");
+const FORMATING = { spaces: 4, EOL: '\r\n' };
 
 /**
  * Writes a json file with the given input
  * @param JSON
  */
 module.exports.writeJson = async (FILE_NAME, JSON) => {
-    return await jsonfile.writeFile(FILE_NAME, JSON);
+    return await jsonfile.writeFile(FILE_NAME, JSON, FORMATING);
 };
