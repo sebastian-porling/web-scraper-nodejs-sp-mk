df4dca813bc32bb9e39f0f4b203e051384e3735a
Sebastian Porling <sebastian.porling@gmail.com>
2020-08-21 20:52:11 +0200

    feat: optimized the async foreach fetch

--- index.js
+++ index.js
@@ -5,36 +5,37 @@ const {
     scrapePresidents,
 } = require("./modules/webScraper.js");
 const { writeJson } = require("./modules/fileWriter.js");
+const throbber = ora();
+const output = [];
 
 /**
  * Fetches information of presidents from the USA
  * and writes it out into a json file.
  */
 module.exports.main = async () => {
-    const output = [];
-    const throbber = ora();
     try {
         throbber.start(
-            "Feching presidents from /wiki/List_of_presidents_of_the_United_States"
+            "Feching presidents from /wiki/List_of_presidents_of_the_United_States\n"
         );
         const response = await getPresidents();
         const presidents = await scrapePresidents(response.data);
-        throbber
-            .succeed(" All done fetching presidents!\n")
-            .start("Fetching information from each president...");
-        presidents.forEach(async (president, i, arr) => {
-            const response = await getPresident(president.link);
-            const { born, died } = await scrapePresident(response.data);
-            output.push(Object.assign(president, { born, died }));
-            if (i === arr.length - 1) {
-                throbber
-                    .succeed(" All president information is fetched!\n")
-                    .start("Writing to presidents.json...");
-                await writeJson("presidents.json", output);
-                throbber.succeed(" Done writing, check presidents.json!");
-            }
-        });
+        throbber.succeed("Done fetching president list!\n")
+                .start("Fetching each presidents information!\n")
+        await addPresidentInfromation(presidents);
+        throbber.succeed("All president information is fetched!\n")
+                .start("Writing to presidents.json...\n");
+        await writeJson("presidents.json", output);
+        throbber.succeed("Done writing, check presidents.json!");
     } catch (err) {
         throbber.fail("We recieved error while fetching or writing json file!");
     }
 };
+
+const addPresidentInfromation = async (presidents) => {
+    await presidents.reduce(async (promise, president) => {
+        const response = await getPresident(president.link);
+        throbber.start("Fetced " + president.link + "\n")
+        const { born, died } = await scrapePresident(response.data);
+        output.push(await Object.assign(president, { born, died }));
+    }, Promise.resolve());
+}
