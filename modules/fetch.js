95a9af1969a2819a6de1780406147abe0869df89
Sebastian Porling <sebastian.porling@gmail.com>
2020-08-21 22:42:52 +0200

    update: changed constant names

--- modules/fetch.js
+++ modules/fetch.js
@@ -1,19 +1,19 @@
 //const request = require("request-promise");
 const axios = require("axios");
-const presidentsPath = "/wiki/List_of_presidents_of_the_United_States";
-const wikiURL = "https://en.wikipedia.org";
+const PRESIDENT_LIST_PAGE = "/wiki/List_of_presidents_of_the_United_States";
+const WIKI_URL = "https://en.wikipedia.org";
 
 /**
  * Fetches the USA president page on Wikipedia
  */
 module.exports.getPresidents = async () => {
-    return await axios.get(wikiURL + presidentsPath);
+    return await axios.get(WIKI_URL + PRESIDENT_LIST_PAGE);
 };
 
 /**
  * Fetches the HTML content for a wikipage
  * @param presidentPath wiki path for a president
  */
-module.exports.getPresident = async (presidentPath) => {
-    return await axios.get(wikiURL + presidentPath);
+module.exports.getPresident = async (PRESIDENT_PAGE) => {
+    return await axios.get(WIKI_URL + PRESIDENT_PAGE);
 };
