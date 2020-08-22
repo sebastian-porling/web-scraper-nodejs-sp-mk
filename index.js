daf23c35b35befb6021b231ad32b8aeb34f0684d
Sebastian Porling <sebastian.porling@gmail.com>
2020-08-22 22:53:55 +0200

    feat: better async map fetch, new error log

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const ora = require("ora");
-const { getPresident, getPresidents } = require("./modules/fetch.js");
+const {
+    getPresident,
+    getPresidents,
+    PRESIDENT_LIST_PAGE,
+} = require("./modules/fetch.js");
 const {
     scrapePresident,
     scrapePresidents,
@@ -14,7 +18,7 @@ const throbber = ora();
 module.exports.main = async () => {
     try {
         throbber.start(
-            "Feching presidents from /wiki/List_of_presidents_of_the_United_States\n"
+            "Feching presidents from " + PRESIDENT_LIST_PAGE + " \n"
         );
         const response = await getPresidents();
         const presidents = await scrapePresidents(response.data);
@@ -28,24 +32,25 @@ module.exports.main = async () => {
         output.sort((first, second) => first.number - second.number);
         await writeJson("presidents.json", output);
         throbber.succeed("Done writing, check presidents.json!");
-    } catch (err) {
-        throbber.fail("We recieved error while fetching or writing json file!\n\t" + err);
+    } catch (error) {
+        throbber.fail(error);
     }
 };
 
 /**
  * Adds information about each president.
- * @param presidents
+ * @param presidents list of presidents
  */
 const addPresidentInfromation = async (presidents = []) => {
-    if ( presidents.length === 0 ) throw "The array of presidents is empyty";
+    if (presidents.length === 0) throw "The array of presidents is empty";
     const result = [];
-    await presidents.reduce(async (promise, president) => {
-        const response = await getPresident(president.link);
-        throbber.start("Fetched " + president.link + "\n");
-        const presidentInfo = await scrapePresident(response.data);
-        result.push(await Object.assign(president, presidentInfo));
-        await promise;
-    }, Promise.resolve());
+    await Promise.all(
+        presidents.map(async (president) => {
+            const response = await getPresident(president.link);
+            throbber.start("Fetched " + president.link + "\n");
+            const presidentInfo = await scrapePresident(response.data);
+            result.push(await Object.assign(president, presidentInfo));
+        })
+    );
     return result;
 };
